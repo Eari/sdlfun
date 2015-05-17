892bfae3843c4cff0a45abd57831410373c0fbdd
Eari359 <antonin.minarik1@gmail.com>
2015-05-17 23:05:19 +0000

    eeee

--- ubuntu-prepare.sh
+++
@@ -1,8 +0,0 @@
-# My personal preparation script for live ubuntu
-
-apt-get install -y -q=2 gdebi ubuntu-restricted-extras
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-gdebi --n google-chrome-stable_current_amd64.deb
-rm -f google-chrome-stable_current_amd64.deb
-apt-get install -y -q=2 vim vlc
-
